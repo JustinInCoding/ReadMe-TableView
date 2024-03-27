3b5873d1ab82c232b2dd74750d82020df2b8208b
JustinSheng <justin.wang4@icloud.com>
2024-03-27 18:47:26 +0800

    dequeuing cells

--- ReadMe/Library.swift
+++ ReadMe/Library.swift
@@ -32,25 +32,47 @@
 
 import UIKit
 
-class LibraryViewController: UITableViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
+// MARK:- Reusable SFSymbol Images
+enum LibrarySymbol {
+  case bookmark
+  case bookmarkFill
+  case book
+  case letterSquare(letter: Character?)
+  
+  var image: UIImage {
+    let imageName: String
+    switch self {
+    case .bookmark, .book:
+      imageName = "\(self)"
+    case .bookmarkFill:
+      imageName = "bookmark.fill"
+    case .letterSquare(let letter):
+      guard
+        let letter = letter?.lowercased(),
+        let image = UIImage(systemName: "\(letter).square")
+      else {
+        imageName = "square"
+        break
+      }
+      return image
+    }
+    return UIImage(systemName: imageName)!
   }
-	
-	// MARK: - DataSource
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		Library.books.count
-	}
-	
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
-		let book = Library.books[indexPath.row]
-		cell.textLabel?.text = book.title
-		cell.imageView?.image = book.image
-		return cell
-	}
-
-
 }
 
+// MARK:- Library
+enum Library {
+  static let books: [Book] = [
+    Book(title: "Ein Neues Land", author: "Shaun Tan"),
+    Book(title: "Bosch", author: "Laurinda Dixon"),
+    Book(title: "Dare to Lead", author: "Brené Brown"),
+    Book(title: "Blasting for Optimum Health Recipe Book", author: "NutriBullet"),
+    Book(title: "Drinking with the Saints", author: "Michael P. Foley"),
+    Book(title: "A Guide to Tea", author: "Adagio Teas"),
+    Book(title: "The Life and Complete Work of Francisco Goya", author: "P. Gassier & J Wilson"),
+    Book(title: "Lady Cottington's Pressed Fairy Book", author: "Lady Cottington"),
+    Book(title: "How to Draw Cats", author: "Janet Rancan"),
+    Book(title: "Drawing People", author: "Barbara Bradley"),
+    Book(title: "What to Say When You Talk to Yourself", author: "Shad Helmstetter")
+  ]
+}
