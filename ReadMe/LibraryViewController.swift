601fda8ae0c949c63a854ea6821ebe934bdcefbe
JustinSheng <justin.wang4@icloud.com>
2024-03-28 09:43:04 +0800

    Add the Add New Book Cell

--- ReadMe/LibraryViewController.swift
+++ ReadMe/LibraryViewController.swift
@@ -38,7 +38,7 @@ class LibraryViewController: UITableViewController {
 		guard let indexPath = tableView.indexPathForSelectedRow else {
 			fatalError("Nothing selected!")
 		}
-		let book = Library.books[indexPath.row]
+		let book = Library.books[indexPath.row - 1]
 		return DetailViewController(coder: coder, book: book)
 	}
 	
@@ -53,14 +53,18 @@ class LibraryViewController: UITableViewController {
 	
 	// MARK: - DataSource
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		Library.books.count
+		Library.books.count + 1
 	}
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if indexPath == IndexPath(row: 0, section: 0) {
+			let cell = tableView.dequeueReusableCell(withIdentifier: "NewBookCell", for: indexPath)
+			return cell
+		}
 		guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(BookCell.self)", for: indexPath) as? BookCell else {
 			fatalError("Could not create BookCell")
 		}
-		let book = Library.books[indexPath.row]
+		let book = Library.books[indexPath.row - 1]
 		cell.titleLabel.text = book.title
 		cell.authorLabel.text = book.author
 		cell.bookThumbnail.image = book.image
