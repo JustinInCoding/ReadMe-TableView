fa4cbc425ed07f90cab06df89afe7f3f14adff4b
JustinSheng <justin.wang4@icloud.com>
2024-03-28 10:21:53 +0800

    refactor DetailViewController with static table view cells

--- ReadMe/DetailViewController.swift
+++ ReadMe/DetailViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UITableViewController {
 	
 	let book: Book
 	
 	@IBOutlet var titleLabel: UILabel!
 	@IBOutlet var authorLabel: UILabel!
 	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var reviewTextView: UITextView!
 	
 	@IBAction func updateImage() {
 		let imagePicker = UIImagePickerController()
@@ -30,6 +31,12 @@ class DetailViewController: UIViewController {
 		imageView.layer.cornerRadius = 16
 		titleLabel.text = book.title
 		authorLabel.text = book.author
+		
+		if let review = book.review {
+			reviewTextView.text = review
+		}
+		
+		reviewTextView.addDoneButton()
 	}
 	
 	
@@ -50,5 +57,22 @@ extension DetailViewController: UIImagePickerControllerDelegate, UINavigationCon
 		imageView.image = selectedImage
 		Library.saveImage(selectedImage, forBook: book)
 		dismiss(animated: true)
+	}
+}
+
+extension DetailViewController: UITextViewDelegate {
+	func textViewDidEndEditing(_ textView: UITextView) {
+		textView.resignFirstResponder()
+	}
+}
+
+extension UITextView {
+	func addDoneButton() {
+		let toolbar = UIToolbar()
+		toolbar.sizeToFit()
+		let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+		let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.resignFirstResponder))
+		toolbar.items = [flexSpace, doneButton]
+		self.inputAccessoryView = toolbar
 	}
 }
